b47c39e0537fbbd165ab169551f84100cbc56156
micah-jaffe <micah.jaffe95@gmail.com>
2018-12-03 14:08:50 -0800

    Complete basic reducer setup

--- todos/frontend/todos.jsx
+++ todos/frontend/todos.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import store from './store/store';
+console.log(store, "hello")
+window.store = store
 
 document.addEventListener("DOMContentLoaded", () => {
 	const root = document.getElementById("root");
