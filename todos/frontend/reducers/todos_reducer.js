aee76a0a38711b6b2b8298c3fd376ca57c05134f
micah-jaffe <micah.jaffe95@gmail.com>
2018-12-03 14:52:37 -0800

    Build out receiveTodo and receiveTodos functionality to update state

--- todos/frontend/reducers/todos_reducer.js
+++ todos/frontend/reducers/todos_reducer.js
@@ -1,5 +1,28 @@
-const todosReducer = (state = {}, action) => {
+import { RECEIVE_TODOS, RECEIVE_TODO, receiveTodo, receiveTodos } from '../actions/todo_actions';
+
+const initialState = {
+  1: {
+    id: 1,
+    title: 'wash car',
+    body: 'with soap',
+    done: false
+  },
+  2: {
+    id: 2,
+    title: 'wash dog',
+    body: 'with shampoo',
+    done: true
+  },
+};
+
+const todosReducer = (state = initialState, action) => {
+  Object.freeze(state);
   switch(action.type) {
+    case RECEIVE_TODOS:
+      return action.todos;
+    case RECEIVE_TODO:
+      const newObj = {[action.todo.id]: action.todo};
+      return Object.assign({}, state, newObj);
     default:
       return state;
   }
--- todos/frontend/todos.jsx
+++ todos/frontend/todos.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import store from './store/store';
-console.log(store, "hello")
-window.store = store
+import { RECEIVE_TODOS, RECEIVE_TODO, receiveTodo, receiveTodos } from './actions/todo_actions';
+
+window.store = store;
+window.receiveTodo = receiveTodo;
+window.receiveTodos = receiveTodos;
+
 
 document.addEventListener("DOMContentLoaded", () => {
 	const root = document.getElementById("root");
